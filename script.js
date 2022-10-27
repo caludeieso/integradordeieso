2e1d761e820ddd4a8ca20ea2128c9c0019b7892e
calu <caludeieso@gmail.com>
2022-10-27 14:03:32 -0300

    completo

--- script.js
+++ script.js
@@ -9,34 +9,66 @@ let descuentoJunior= 0.85;
 let nombre= document.getElementById("nombre");
 let apellido= document.getElementById("apellido");
 let mail= document.getElementById("mail");
-let cantidadT= document.getElementById("cantidadTickets").value;
-let categoria= document.getElementById("categoriaSelect");
 
 
-function total_a_pagar() {  
-   
-    if (categoria.value==0){
-     resumen=valorTicket*cantidadT;
-      return ;  
+
+function total_a_pagar() { 
+  const valorTicket = 200;
+  let cantidadT= document.getElementById("cantidadTickets").value;
+  let categoria= document.getElementById("categoriaSelect").value;
+  var resumen=0 
+  //validaciones
+  if (nombre.value==="" || apellido.value==="" || mail.value===""||categoria<"0") {
+       alert("Completar campos obligatorios"); 
+       document.getElementById("obligatorio").innerText="*Completar campos requeridos" 
+       return resumen=0;
+  }
+  
+  const emailValido = mail => 
+     {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
+     }
+     if(!emailValido(mail.value))
+     {
+        alert("Escribir bien el mail...");
+        return ;
+     }
+
+  //calculo
+
+    if (categoria==0){
+     resumen=valorTicket*cantidadT; 
     } 
-    if (categoria.value==1){
+    if (categoria==1){
        resumen=valorTicket*descuentoEstudiante*cantidadT;
-      return ;  
     } 
-    if (categoria.value==2){
-        resumen=valorTicket*descuentoTrainee*cantidadT;
-      return ;  
+    if (categoria==2){
+        resumen=valorTicket*descuentoTrainee*cantidadT;   
     } 
-    if (categoria.value==3){
-      resumen=valorTicket*descuentoJunior*cantidadT;
-      return ;  
+    if (categoria==3){
+      resumen=valorTicket*descuentoJunior*cantidadT;   
     } 
-    
-  return document.getElementById("totalResumen").value=resumen;
+  
+   document.getElementById("totalResumen").innerText=resumen;
+   if (resumen>0) {
+    document.getElementById("obligatorio").innerText="" 
+   }
    
 }
 
 btnResumen.addEventListener("click",total_a_pagar);
+
+function borrar() {
+ document.getElementById("nombre").value="";
+ document.getElementById("apellido").value="";
+ document.getElementById("mail").value="";
+ document.getElementById("cantidadTickets").value="";
+ document.getElementById("categoriaSelect").value="";
+document.getElementById("totalResumen").innerText="";
+}
+
+document.getElementById("btnBorrar").addEventListener("click",borrar);
+
 
 
 
